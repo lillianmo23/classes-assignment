dfa9a08e93c59e9cdf5eb22e47fa8440de6d1a4a
lillianmo23 <lillian.mo23@ycdsbk12.ca>
2022-04-07 22:49:54 -0400

    fixing super apple

--- superapple/draw_super_apple.py
+++ superapple/draw_super_apple.py
@@ -1,22 +1,34 @@
 import pygame
 from random import randint
 
+WIDTH = 640
+HEIGHT = 480
+SIZE = (WIDTH, HEIGHT)
+
+screen = pygame.display.set_mode(SIZE)
 class SuperApple:
-    def __init__(self, dx:int, dy:int) -> None:
-        self.dx = dx
-        self.dy = dy
 
+    sup_app_img = pygame.image.load("super_apple.png")
+    sup_app_img_rect = sup_app_img.get_rect()
 
-    def draw_super_apple(self, surface):
-        pygame.draw.circle(surface)
-        pass  
-
-class SuperAppleImg:
-    def __init__(self, x: int, y:int, dx:int, dy:int) -> None:
+    def __init__(self, x: int, y: int, colour:pygame.Color, radius, factor) -> None:
         self.x = x
         self.y = y
-        self.dx = dx
-        self.dy = dy
+        self.colour = colour
+        self.radius = radius
+        self.factor = factor
     
-    def super_apple_draw():
-        pass
+    def transform_apples(self, image):
+        global scaled, scaled_rect
+        scaled = pygame.transform.scale(image, self.factor, self.factor/10)
+        scaled_rect = scaled.get_rect()
+        return scaled
+    
+    def draw_it(self, surface):
+        screen.blit(scaled, (self.x, self.y))
+        pygame.draw.circle(surface, self.colour, (self.x, self.y), self.radius)
+    
+def super_apple():
+    return SuperApple(randint(80, 580), randint(80, 380), (255, 0, 0), randint(50, 100), randint(0, 4))
+
+
--- superapple/super_apple.py
+++ superapple/super_apple.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.draw import *
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 from random import randint
+from draw_super_apple import SuperApple, super_apple
 
 pygame.init()
 
@@ -12,33 +13,11 @@ SIZE = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
+apples = [super_apple()]
+sup_app_img = pygame.image.load("super_apple.png")
 
-class SuperApple:
-
-    sup_app_img = pygame.image.load("super_apple.png")
-
-    def __init__(self, x: int, y: int, dx: int, dy: int) -> None:
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-    
-    def transform_apples(image, x:int, y:int):
-        scaled = pygame.transform.scale(image, x, y)
-        x_factor = round(50/x, 2)
-        y_factor = round(50/y, 2)
-        return scaled, x_factor, y_factor
-
-    apple_width = randint(10, 100)
-    apple_height = randint(10, 100)
-
-apple_draw = SuperApple()
-
-health = 20
 running = True
-while health>0:
-    running = True
-
+time = 0
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -53,7 +32,12 @@ while running:
 
     # DRAWING
     screen.fill((255, 255, 255)) # always the first drawing command
-    screen.blit(SuperApple()) 
+    time += 1
+    if time%120 == 0:
+        apples.append(super_apple())
+    
+    for apple in apples:
+        SuperApple.draw_it(self, screen)
 
     # Must be the last two lines
     # of the game loop
