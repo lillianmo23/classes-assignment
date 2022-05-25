84a8db90e4b805eab217d64618371cc534daec3f
lillianmo23 <lillian.mo23@ycdsbk12.ca>
2022-05-25 11:24:05 -0400

    final commit

--- superapple/draw_super_apple.py
+++ superapple/draw_super_apple.py
@@ -1,34 +1,66 @@
 import pygame
 from random import randint
+from math import sqrt
 
-WIDTH = 640
-HEIGHT = 480
-SIZE = (WIDTH, HEIGHT)
+sup_app_img = pygame.image.load("super_apple.png")
+sup_app_img_rect = sup_app_img.get_rect()
 
-screen = pygame.display.set_mode(SIZE)
+#class for the inside of the apple
+class InnerApple:
+    def __init__(self, x, y, factor) -> None:
+        self.x = x
+        self.y = y
+        self.factor = factor
+
+#the main class for the entire super apple
 class SuperApple:
 
-    sup_app_img = pygame.image.load("super_apple.png")
-    sup_app_img_rect = sup_app_img.get_rect()
-
-    def __init__(self, x: int, y: int, colour:pygame.Color, radius, factor) -> None:
+    def __init__(self, x, y, colour, radius, image) -> None:
         self.x = x
         self.y = y
         self.colour = colour
         self.radius = radius
-        self.factor = factor
+        self.factor = 0.01
+        self.image = pygame.transform.scale(image, (self.factor, self.factor))
     
-    def transform_apples(self, image):
-        global scaled, scaled_rect
-        scaled = pygame.transform.scale(image, self.factor, self.factor/10)
-        scaled_rect = scaled.get_rect()
-        return scaled
-    
-    def draw_it(self, surface):
-        screen.blit(scaled, (self.x, self.y))
+    def draw_it(self, surface:pygame.Surface) -> None:
+        """draws the super apple
+
+        Args:
+            surface (Surface): the surface of the image
+        """
+        surface.blit(self.image, (self.x, self.y))
         pygame.draw.circle(surface, self.colour, (self.x, self.y), self.radius)
+
+    def grow(self, amount:int) -> None:
+        """makes the super apple bigger
+
+        Args:
+            amount (int): the amount to grow the super apple by
+        """
+        self.radius += amount
+        scale = 0
+
+    def shrink(self, amount:int) -> None:
+        """makes the super apple smaller
+
+        Args:
+            amount (int): the amount to shrink the super apply by
+        """
+        self.radius -= amount
     
-def super_apple():
-    return SuperApple(randint(80, 580), randint(80, 380), (255, 0, 0), randint(50, 100), randint(0, 4))
+    def distance(self, point:int) -> None:
+        global click_dist
+        click_dist = sqrt((point[0]-self.x)^2+(point[2]-self.y)^2)
+
+
+    @staticmethod
+    def get() -> object:
+        """generates a random super apple
+
+        Returns:
+            SuperApple (object): a super apple with randomly generated location and size
+        """
+        return SuperApple(randint(80, 480), randint(80, 380), (0, 0, 255), randint(0, 100), sup_app_img)
 
 
--- superapple/main.py
+++ superapple/main.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.draw import *
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 from random import randint
-from draw_super_apple import SuperApple, super_apple
+from draw_super_apple import SuperApple
 
 pygame.init()
 
@@ -13,8 +13,8 @@ SIZE = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
-apples = [super_apple()]
-sup_app_img = pygame.image.load("super_apple.png")
+#makes a list for the super apples
+apples = [SuperApple.get()]
 
 running = True
 time = 0
@@ -32,12 +32,14 @@ while running:
 
     # DRAWING
     screen.fill((255, 255, 255)) # always the first drawing command
+
+    #adds a new super apple every 3 seconds
     time += 1
     if time%120 == 0:
-        apples.append(super_apple())
+        apples.append(SuperApple.get())
     
     for apple in apples:
-        SuperApple.draw_it(self, screen)
+        apple.draw_it(screen)
 
     # Must be the last two lines
     # of the game loop
